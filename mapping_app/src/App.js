7fdc4aace8979435a6f9ab0aacc00de4803d155c
Chandler Puckett <chandler.puckett1@gmail.com>
2021-06-05 23:27:41 -0400

    quick commit

--- mapping_app/src/App.js
+++ mapping_app/src/App.js
@@ -7,21 +7,31 @@ import { useState } from 'react'
 const App = () => {
   const [tasks, setTasks] = useState([
     { text: 'this is text 1',
-      id: 1
+      id: 1,
+      day: 'day1'
     }, 
     { text: 'this is text 2',
-      id: 2
+      id: 2,
+      day: 'day2'
     },
     { text: 'this is text 3',
-      id: 3
+      id: 3,
+      day: 'day3'
     }
   ])
 
+  const deleteTask = (id) => {
+    console.log('delete', id)
+    setTasks(tasks.filter((task) => task.id !== id))
+  }
 
   return (
     <div className="container">
       <Header />
-      <Tasks tasks={tasks}/>
+      { tasks.length > 0 ? 
+        (<Tasks tasks={tasks} onDelete={deleteTask}/>) :
+        ('420 blaze it skkrr skrrr!!!!!!')  
+      }
     </div>
   );
 }
--- mapping_app/src/components/Tasks.js
+++ mapping_app/src/components/Tasks.js
@@ -1,10 +1,13 @@
+import Task from './Task'
 
-const Tasks = ({tasks}) => {
+const Tasks = ({tasks, onDelete}) => {
   
   return (
     // use setTasks as a "setter" to mutate the data
     <>
-      {tasks.map((task)=> (<h3 key={task.id}>{task.text}</h3>))}
+      {tasks.map((task)=> (
+        <Task key={task.id} task={task} onDelete={onDelete}/>
+      ))}
     </>
   )
 }
